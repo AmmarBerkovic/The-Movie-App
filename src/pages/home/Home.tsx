84bf8d831ba027220803d94c9f1e656964d55afb
Ammar Zefir <ammar.berkovic@gmail.com>
2024-05-17 18:54:49 +0200

    Tiles styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./home.scss";
 import Navigation from "../../components/navigation/Navigation";
 import SearchBar from "../../components/search-bar/SearchBar";
 import Articles from "../../components/articles/Articles";
